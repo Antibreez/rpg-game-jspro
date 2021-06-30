97d36ba09963d767a5c308265ebc0f287ba2e122
Anton Kiyan <kiyananton@gmail.com>
2021-06-30 14:17:17 +0300

    modify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+const NODE_ENV = process.env.NODE_ENV;
+
 module.exports = {
-  mode: 'development',
+  mode: NODE_ENV || 'development',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  watch: true,
+  watch: NODE_ENV === 'development',
   watchOptions: {
     ignored: /node_modules/,
     poll: 1000,
@@ -25,8 +27,8 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader',
-          'sass-loader'
-        ]
+          'sass-loader',
+        ],
       },
       {
         test: [/\.svg$/, /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
@@ -36,10 +38,10 @@ module.exports = {
             options: {
               name: '[name].[ext]',
               outputPath: 'assets/',
-            }
-          }
-        ]
-      }
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
@@ -54,4 +56,5 @@ module.exports = {
     hot: true,
     historyApiFallback: true,
   },
+  devtool: NODE_ENV === 'development' ? 'source-map' : false,
 };
