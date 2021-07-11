5fb3cb9521eb234c67add6c6cc0f41033103e432
Anton Kiyan <kiyananton@gmail.com>
2021-07-11 21:42:37 +0300

    homework-3

--- src/common/EventSourceMixin.js
+++ src/common/EventSourceMixin.js
@@ -1,23 +1,23 @@
 export default {
-  pushEvent (event, sub) {
+  pushEvent(event, sub) {
     const subs = this.subscribers || (this.subscribers = {});
     (subs[event] || (subs[event] = [])).push(sub);
   },
 
-  on (event, callback) {
+  on(event, callback) {
     this.pushEvent(event, [true, callback]);
   },
 
-  once (event, callback) {
+  once(event, callback) {
     this.pushEvent(event, [false, callback]);
   },
 
-  un (event, subToUn) {
+  un(event, subToUn) {
     const subs = this.subscribers;
     if (subs && subs[event]) subs[event] = subs[event].filter((sub) => sub !== subToUn);
   },
 
-  trigger (event, data = null) {
+  trigger(event, data = null) {
     const subs = this.subscribers;
     if (subs && subs[event]) {
       // вызываем все обработчики
